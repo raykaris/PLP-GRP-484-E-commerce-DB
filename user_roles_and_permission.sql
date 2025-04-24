409e7289d1e65b77c023f79814f79a2e403ed1a7
raykaris <ryanmuthoni050@gmail.com>
2025-04-24 15:18:51 -0400

    user_role_&_permission update

---
+++ user_roles_and_permission.sql
@@ -0,0 +1,27 @@
+-- Create roles
+CREATE ROLE ecommerce_admin;
+CREATE ROLE ecommerce_staff;
+CREATE ROLE ecommerce_viewer;
+
+-- Grant privileges to roles
+GRANT ALL PRIVILEGES ON GRP_484_ecommerce.* TO ecommerce_admin;
+GRANT SELECT, INSERT, UPDATE ON GRP_484_ecommerce.* TO ecommerce_staff;
+GRANT SELECT ON GRP_484_ecommerce.* TO ecommerce_viewer;
+
+-- Create users
+CREATE USER 'celestine'@'localhost' IDENTIFIED BY 'celestine123';
+CREATE USER 'kariuki'@'localhost' IDENTIFIED BY 'kariuki456';
+CREATE User 'martin'@'localhost' IDENTIFIED BY 'martin789';
+
+-- Assign roles to users
+GRANT ecommerce_admin TO 'celestine'@'localhost';
+GRANT ecommerce_viewer TO 'kariuki'@'localhost';
+GRANT ecommerce_staff TO 'martin'@'localhost';
+
+-- Set default roles
+SET DEFAULT ROLE ecommerce_admin TO 'celestine'@'localhost';
+SET DEFAULT ROLE ecommerce_viewer TO 'kariuki'@'localhost';
+SET DEFAULT ROLE ecommerce_staff TO 'martin'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
